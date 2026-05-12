c703b95c1ee3da4667c2a6040c7629c86b2ecf3e
kasirajubhargav-lab <kasirajubhargav@gmail.com>
2026-05-13 02:11:14 +0530

    Completed JWT Authentication API

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.get("/", (req, res) => {
+  res.send("Auth API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
